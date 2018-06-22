8d4d6d6a5f7690b96c04cf00ef77c5b1fe14d019
Marcelo Alvarez <malvarez@edison09.nersc.gov>
2018-06-22 10:48:38 -0700

    removed snapshot from __init__.py

--- illstack/cystack.pyx
+++ illstack/cystack.pyx
@@ -2,16 +2,44 @@ import healpy as hp
 import numpy as np
 cimport numpy as np
 
-def some_fast_cython_stacking_routine(np.ndarray x, n):
-    
+search_radius = 2
+ntile=2
+def cullonhalos(
+    np.ndarray xp,np.ndarray yp,np.ndarray zp,
+    np.ndarray xh,np.ndarray yh,np.ndarray zh,
+    np.ndarray rh,np.ndarray mh):
+
     '''
-    THIS IS A TEMPLATE FUNCTION FOR CYTHON IN ILLSTACK
     Parameters
-    x[:]: an input array
-    n: 	  some input integer
+	particles[nparticles][npartprops]
+        halos[nphalos][nhaloprops]
 
     Returns
-    the_output[0:n-1]: an output array
+	profiles[:,nhalos]
     '''
-    the_output = np.zeros(n)
-    return the_output
+    
+    nhalos=np.shape(xh)[0]
+    
+    ninhalos=0
+    nphalo = np.zeros(nhalos)
+    nhalos = 10
+    xpin = []; ypin=[]; zpin=[]
+    for ih in np.arange(nhalos):    	
+#        if ih%1 == 1000: print ih,'done out of',nhalos
+        print ih,'done out of',nhalos
+        rp = np.sqrt((xp-xh[ih])**2+(yp-yh[ih])**2+(zp-zh[ih])**2)
+        dm = [np.abs(rp) < rh[ih]]
+
+        xpin.extend(xp[dm])
+        ypin.extend(yp[dm])
+        zpin.extend(zp[dm])
+
+        ninhalos += len(xp[dm])
+
+    xpin = np.asarray(xpin)
+    ypin = np.asarray(ypin)
+    zpin = np.asarray(zpin)
+
+    print 'ninhalos = ',ninhalos
+    print 'size of xpin = ',len(xpin) 
+	
