4bbc2bacdff9b3bfd06ed4fc647c72816ddc3bad
Emily Moser <emilymoser@Emilys-MacBook-Air-2.local>
2019-06-16 17:53:28 -0400

    changed format of getprof_scaled to input central mass instead of low and high

--- example/getprof_scaled.sh
+++ example/getprof_scaled.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Columns: 1 init, 2 profile type, 3 lower mass limit, 4 upper mass limit, 5 snapshot (redshift) 
-python ../scripts/profiles_scaled.py istk-params_scaled.txt dmdens 8e11 1.2e12 084 # dm
-python ../scripts/profiles_scaled.py istk-params_scaled.txt gasdens 8e11 1.2e12 084 # gas
-python ../scripts/profiles_scaled.py istk-params_scaled.txt gaspth 8e11 1.2e12 084   # gas
-python ../scripts/showstacks_scaled.py stack_gasdens_scaled_12.npz stack_gaspth_scaled_12.npz 8e11 1.2e12 dens
+#python ../scripts/profiles_scaled.py istk-params_scaled.txt dmdens 1e13 120 # dm
+#python ../scripts/profiles_scaled.py istk-params_scaled.txt gasdens 1e13 120 # gas
+#python ../scripts/profiles_scaled.py istk-params_scaled.txt gaspth 1e13 120   # gas
+python ../scripts/showstacks_scaled.py stack_gasdens_scaled_ill_13.npz stack_gaspth_scaled_ill_13.npz 1e13 pres
