0edfed240ab5c125fdf170fca3bb1384929063ab
Emily Moser <emilymoser@Emilys-MacBook-Air-2.local>
2019-06-16 21:06:35 -0400

    not hard-coded at snap 135

--- illstack/io.py
+++ illstack/io.py
@@ -5,16 +5,18 @@ import params
 def getparticles(snapshot_number,partType,field_list):
 
     basePath=params.basepath
-    particles = il.snapshot.loadSubset(basePath,135,partType,fields=field_list)
+    particles = il.snapshot.loadSubset(basePath,snapshot_number,partType,fields=field_list)
 
     return particles
 
 def gethalos(snapshot_number,field_list):
     
     basePath=params.basepath
-    return il.groupcat.loadHalos(basePath,135,fields=field_list)
+    return il.groupcat.loadHalos(basePath,snapshot_number,fields=field_list)
 
 
-
+def getsubhalos(snapshot_number,field_list):
+    basePath=params.basepath
+    return il.groupcat.loadSubhalos(basePath, snapshot_number,fields=field_list)
 
 
