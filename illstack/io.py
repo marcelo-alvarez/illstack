1dc80d1a74f75223e6bb345de82f37fb6e512f22
Emily Moser <emm376@cori12.nersc.gov>
2019-07-15 10:43:13 -0700

    io needs path appended to work in NERSC

--- illstack/io.py
+++ illstack/io.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0,'/global/cscratch1/sd/emm376/')
 import illustris_python as il
 import numpy as np
 import params
@@ -5,15 +7,18 @@ import params
 def getparticles(snapshot_number,partType,field_list):
 
     basePath=params.basepath
-    particles = il.snapshot.loadSubset(basePath,135,partType,fields=field_list)
+    particles = il.snapshot.loadSubset(basePath,snapshot_number,partType,fields=field_list)
 
     return particles
 
 def gethalos(snapshot_number,field_list):
     
     basePath=params.basepath
-    return il.groupcat.loadHalos(basePath,135,fields=field_list)
+    return il.groupcat.loadHalos(basePath,snapshot_number,fields=field_list)
 
+def getsubhalos(snapshot_number,field_list):
+    basePath=params.basepath
+    return il.groupcat.loadSubhalos(basePath, snapshot_number,fields=field_list)
 
 
 
