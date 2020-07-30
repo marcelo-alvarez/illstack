b0c6a3c6ff6ab4daabcec02a09829c848191b4a1
Emily Moser <emm376@cornell.edu>
2020-07-30 14:03:45 -0400

    fix print statements

--- scripts/profiles_mult.py
+++ scripts/profiles_mult.py
@@ -21,12 +21,12 @@ mass_kind=params_tng.mass_kind #this is either 'halo' or 'stellar'
 if mass_option == 1:
     mlow=params_tng.mass_low
     mhigh=params_tng.mass_high
-    print "Proceeding with mass option 1 for %s mass: upper/lower mass range, %s < M < %s"%(mass_kind,str(mlow),str(mhigh))
+    print("Proceeding with mass option 1 for %s mass: upper/lower mass range, %s < M < %s"%(mass_kind,str(mlow),str(mhigh)))
 elif mass_option == 2:
     percentage = params_tng.mass_option_percent
     mlow = (1.0-percentage)*mcenter
     mhigh = (1.0+percentage)*mcenter
-    print "Proceeding with mass option 2 for %s mass: central mass and %s percent ranges,%s < M < %s"%(mass_kind,100*percentage,str(mlow),str(mhigh))
+    print("Proceeding with mass option 2 for %s mass: central mass and %s percent ranges,%s < M < %s"%(mass_kind,100*percentage,str(mlow),str(mhigh)))
 
 mhmin = mlow /1e10 # minimum mass in 1e10 Msun/h
 mhmax = mhigh /1e10 # maximum mass in 1e10 Msun/h
@@ -46,31 +46,31 @@ mcenter_power=np.log10(float(mcenter))
 mcenter_power=str(int(mcenter_power))
 
 if prof=='gasdens':
-    print "Completing profiles for gasdens"
+    print("Completing profiles for gasdens")
     part_type='gas'
     field_list = ['Coordinates','Masses']
     gas_particles = istk.io.getparticles(snap_num,part_type,field_list) #Change redshift
     posp = gas_particles['Coordinates'] #position, base unit ckpc/h 
     vals = gas_particles['Masses']   #units 1e10 Msol/h
 elif prof=='dmdens':
-    print "Completing profiles for dmdens"
+    print("Completing profiles for dmdens")
     part_type='dm'
     # HARD CODED BOX SIZE 7.5e4 kpc/h
     part_massf=2.775e2*omegadm*(7.5e4)**3/1e10 # particle mass in 1e10 Msun/h
     field_list = ['Coordinates'] #base unit ckpc/h
     posp = istk.io.getparticles(snap_num,part_type,field_list) #Change redshift
     vals = posp[:,0]*0 + part_massf / np.shape(posp)[0]
-    print 'setting dm particle mass to = ',vals[0]*1e10,'Msun/h'
+    print('setting dm particle mass to = ',vals[0]*1e10,'Msun/h')
 elif prof=='gaspth':
-    print "Completing profiles for gaspth"
+    print("Completing profiles for gaspth")
     part_type='gas'
     field_list = ['Coordinates','Masses','InternalEnergy']
     gas_particles = istk.io.getparticles(snap_num,part_type,field_list) #Change redshift
     posp = gas_particles['Coordinates'] #base unit ckpc/h 
     vals = gas_particles['Masses']*gas_particles['InternalEnergy']*(gamma-1.) #unit 1e10Msol/h,(km/s)**2
 else:
-    print "Please enter an appropriate option for the profile"
-    print "gasdens,dmdens,gaspth"
+    print("Please enter an appropriate option for the profile")
+    print("gasdens,dmdens,gaspth")
 
 
 field_list = ['Group_M_Crit200','GroupPos','Group_R_Crit200','GroupFirstSub', 'GroupSFR','GroupMassType']
@@ -84,18 +84,16 @@ sfr  = halos['GroupSFR'] #Msol/yr
 halo_mass= halos['GroupMassType']
 mstar= halo_mass[:,4] #stellar mass, 1e10 Msol/h
 
-print "orignal posh", np.shape(posh)
-print "shape of groupfirstsub", np.shape(GroupFirstSub)
+#print "orignal posh", np.shape(posh)
+#print "shape of groupfirstsub", np.shape(GroupFirstSub)
 
 r, val, n, mh, rh, nprofs,GroupFirstSub,sfr,mstar = istk.cyprof.stackonhalos(posp,vals,posh,mh,rh,GroupFirstSub,sfr,mstar,ntile,volweight,mhmin, mhmax,scaled_radius,mass_kind)
-print "nprofs", nprofs
+#print "nprofs", nprofs
 r  =np.reshape(r,  (nprofs,istk.params.bins))
 val=np.reshape(val,(nprofs,istk.params.bins)) 
 n  =np.reshape(n,  (nprofs,istk.params.bins))
 
-print 'shapes: ','r', np.shape(r),'val', np.shape(val),'n', np.shape(n),'mh', np.shape(mh)
-
+#print 'shapes: ','r', np.shape(r),'val', np.shape(val),'n', np.shape(n),'mh', np.shape(mh)
 #np.savez(prof+'_scaled_'+sim+'_'+str(sys.argv[4])+'_'+mcenter_power+'.npz',r=r[0],val=val,n=n,mh=mh,rh=rh,nprofs=nprofs,nbins=istk.params.bins,GroupFirstSub=GroupFirstSub,sfr=sfr,mstar=mstar)
-np.savez(prof+'_unscaled_'+sim+'_'+str(sys.argv[4])+'_mstar_total_test100.npz',r=r[0],val=val,n=n,mh=mh,rh=rh,nprofs=nprofs,nbins=istk.params.bins,GroupFirstSub=GroupFirstSub,sfr=sfr,mstar=mstar)
 
-#np.savez('test.npz',r=r[0],val=val,n=n,mh=mh,rh=rh,nprofs=nprofs,nbins=istk.params.bins,GroupFirstSub=GroupFirstSub,sfr=sfr,mstar=mstar)
+np.savez(prof+'_unscaled_'+sim+'_'+str(sys.argv[4])+'_mstar_total_test100.npz',r=r[0],val=val,n=n,mh=mh,rh=rh,nprofs=nprofs,nbins=istk.params.bins,GroupFirstSub=GroupFirstSub,sfr=sfr,mstar=mstar)
